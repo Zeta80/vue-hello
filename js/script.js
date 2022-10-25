cd96159110eed355bf4bbeb5748d7433c4863eab
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-25 17:26:14 +0200

    Finish bonus

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ createApp({
             userLastName: "",
             userNickName: "",
             greetingMessage: "",
+            image: "img/whoareyoulogo.jpg",
+            // imageWhoAre: "img/whoareyoulogo.jpg",
+            imageOhItsYou: "img/post-26450-0-02999100-1402274672.jpg"
+
 
         }
     },
@@ -18,9 +22,12 @@ createApp({
         greet: function () {
             if (this.userNickName) {
                 this.greetingMessage = `Ciao ${this.userName} ${this.userLastName} detto anche ${this.userNickName}!`
+                this.image = this.imageOhItsYou
             } else {
                 this.greetingMessage = `Ciao ${this.userName} ${this.userLastName}!`
+                this.image = this.imageOhItsYou
             }
+
         }
     }
 
